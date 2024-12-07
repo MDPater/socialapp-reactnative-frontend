1116b18989af68eb0674fd4fd9cbdc9a91fa3c23
MDPater <depatermax@gmail.com>
2024-12-07 02:11:21 +0100

    Link App.tsx to auth screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { AuthProvider, useAuth } from './src/context/AuthContext';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/screens/Home';
+import Login from './src/screens/Login';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthProvider>
+      <Layout></Layout>
+    </AuthProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export const Layout = () => {
+  const { authState, onLogout} = useAuth();
+  return (<NavigationContainer>
+    <Stack.Navigator>
+      {authState?.authenticated ? (
+        <Stack.Screen name="Home" component={Home} options={{
+          headerRight: () => <Button onPress={onLogout} title='Sign Out'/>
+        }}></Stack.Screen>
+      ): (<Stack.Screen name='Login' component={Login}></Stack.Screen>)}
+    </Stack.Navigator>
+  </NavigationContainer>);
+}
